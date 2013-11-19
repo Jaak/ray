1f957a925a446831795ac23c2520c83c706a8e1e
Jaak Randmets <jaak.randmets@stacc.ee>
2013-11-19 23:01:17 +0200

    Simplify Pixel.h to not store a boolean.

--- include/pixel.h
+++ include/pixel.h
@@ -2,48 +2,20 @@
 #define RAY_PIXEL_H
 
 #include "geometry.h"
-#include "scene.h"
 
-/**
- * A pixel.
- * Only used to perform anti-aliasing, caching of
- * traced rays specifically.
- * Pixel is active if it is already traced.
- *
- * Basically is kind of: Maybe Colour, but C++ is such
- * a crappy language that i have to write about hundred lines
- * of code and use dynamic memory to get that haskell kind
- * of behaviour of Maybe type.
- */
-class Pixel {
+class Pixel : Colour {
 public: /* Methods: */
-  Pixel(const Colour& c = Colour(0, 0, 0)) : m_col(c), m_active(false) {}
 
-  /**
-   * Sets pixel colour to black and also
-   * deactiavtes it.
-   */
-  void reset() {
-    m_col = { 0, 0, 0 };
-    m_active = false;
-  }
+  Pixel(const Colour& c = Colour(0, 0, 0))
+    : Colour (c)
+  { w = 0; }
 
-  const Colour& colour() const { return m_col; }
-  Colour& colour() { return m_col; }
-  void setColour(const Colour& c) { m_col = c; }
-
-  /// Activates pixel.
-  void activate() { m_active = true; }
-
-  /**
-   * @retval true Pixel is active.
-   * @retval false Pixel is inactive.
-   */
-  bool active() const { return m_active; }
-
-private: /* Fields: */
-  Colour m_col;  ///< Colour of the pixel.
-  bool m_active; ///< Is the pixel active or not.
+  void reset() { r = g = b = w = 0.0; }
+  const Colour& colour() const { return *this; }
+  Colour& colour() { return *this; }
+  void setColour(const Colour& c) { *this = c; }
+  void activate() { w = 1.0; }
+  bool active() const { return w > 0.5; }
 };
 
 #endif
