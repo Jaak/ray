b5db3d3b8adf07dc8a2fa4c4979fe7be084dab5c
Indrek Loolaid <binoternary@gmail.com>
2013-12-09 22:42:14 +0200

    One bug down ...

--- include/spotlight.h
+++ include/spotlight.h
@@ -29,32 +29,14 @@ public: /* Methods: */
     auto H = Vector {0, 0, 0};
     while (true) {
       H = rngHemisphere ();
-      //printf("H.z=%f\n", H.z);
-      if (H.z <= cos (m_angle * (M_PI / 180.0)))
+      if (H.z >= cos (m_angle * (M_PI / 180.0)))
         break;
     }
 
     const auto D = Frame { m_direction }.toWorld (H);
     const auto P = center () + radius () * D;
-    
-    //printf("m_angle %f\n", m_angle);
-    //printf("angle %f\n", getAngle(D, m_direction));
-    //printf("nAngle %f\n", getAngleN(D, m_direction));
 
     return {P.nudgePoint (P), D};
-
-    /*
-    // TODO - need a better way to get a random ray in the right direction
-    while (true) {
-      //const auto dir = rngHemisphereVector(m_direction);
-      const auto dir = rngSphere();
-      // angle between dir and m_direction
-      floating a = getAngleN(m_direction, dir);
-      if (a < m_angle) {
-        return {point.nudgePoint (dir), dir};
-      }
-    } */ 
-
   }
 
   floating lightPA () const {
