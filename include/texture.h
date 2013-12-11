6e268bb590d08293e1b25581f026330687836925
Indrek Loolaid <binoternary@gmail.com>
2013-12-11 15:51:59 +0200

    Bilinear texture filtering

--- include/texture.h
+++ include/texture.h
@@ -14,11 +14,42 @@ public: /* Methods: */
   {}
 
   const Colour& getTexel(floating u, floating v) const {
-    const size_t x = clamp(round(u * width()), 0.0, width() - 1);
-    const size_t y = clamp(round(v * height()), 0.0, height() - 1);
 
-    return (*this)(x, y);
+  /*  if (interpolate) {
+      const size_t x = clamp(round(u * width()), 0.0, width() - 1);
+      const size_t y = clamp(round(v * height()), 0.0, height() - 1);
+
+      return (*this)(x, y);
+    }*/    
+
+    floating x, y, x1, x2, y1, y2;
+    Colour c11, c12, c21, c22;
+
+    x = clamp(u * width(), 0.0, width() - 1);
+    y = clamp(v * height(), 0.0, height() - 1);
+
+    x1 = floor(x); x2 = ceil(x);
+    y1 = floor(y); y2 = ceil(y);
+
+    if (almost_zero(x2 - x1) || almost_zero(y2 - y1)) {
+      return (*this)(round(x), round(y)); 
+    }
+
+    c11 = (*this)(x1, y1);
+    c12 = (*this)(x1, y2);
+    c21 = (*this)(x2, y1);
+    c22 = (*this)(x2, y2);
+
+    const Colour& result = (1.0 / (x2 - x1) * (y2 - y1)) *
+                  (c11 * (x2 - x) * (y2 - y) +
+                   c12 * (x - x1) * (y2 - y) +
+                   c21 * (x2 - x) * (y - y1) +
+                   c22 * (x - x1) * (y - y1)
+                  );
+
+    return result;
   }
+    
 };
 
 
